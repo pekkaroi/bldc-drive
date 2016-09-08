5d5f2a85183df403280ac0933faaa01788b66434
Pekka Roivainen <pekka@roivainen.eu>
2016-09-08 19:48:34 +0300

    adding hall.h

---
+++ firmware/src/hall.h
@@ -0,0 +1,32 @@
+/*
+ 	bldc-drive Cheap and simple brushless DC motor driver designed for CNC applications using STM32 microcontroller.
+	Copyright (C) 2015 Pekka Roivainen
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef HALL_H_
+#define HALL_H_
+
+extern volatile uint16_t lasthallpos;
+extern volatile uint16_t hallpos;
+
+
+void initHALL();
+
+
+#endif /* HALL_H_ */
+
